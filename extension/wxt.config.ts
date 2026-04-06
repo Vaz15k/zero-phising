684eed428061872981d9591aa8636a2fa83965be
Keven Eduardo <79882241+Vaz15k@users.noreply.github.com>
2026-04-06 20:11:22 +0000

    extension: (chore) Configuração do manifest e regras declarativeNetRequest

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -3,4 +3,48 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+
+  manifest: {
+    name: 'ZeroPhishing',
+    description: 'Proteção contra phishing, malware e controle de acesso em tempo real.',
+    version: '0.1.0',
+
+    permissions: [
+      'storage',
+      'tabs',
+      'webNavigation',
+      'declarativeNetRequest',
+      'notifications',
+    ],
+
+    host_permissions: [
+      '<all_urls>',
+    ],
+
+    icons: {
+      16:  'icon/16.png',
+      32:  'icon/32.png',
+      48:  'icon/48.png',
+      96:  'icon/96.png',
+      128: 'icon/128.png',
+    },
+
+    action: {
+      default_popup: 'popup/index.html',
+      default_icon: {
+        16:  'icon/16.png',
+        32:  'icon/32.png',
+        48:  'icon/48.png',
+        128: 'icon/128.png',
+      },
+    },
+
+    declarative_net_request: {
+      rule_resources: [{
+        id: 'blocked',
+        enabled: true,
+        path: 'rules/blocked.json',
+      }],
+    },
+  },
 });
